e0ecfa01952b0f089d5f816d87fce60774b24539
Amogh-Walia <72308844+Amogh-Walia@users.noreply.github.com>
2021-08-17 22:58:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,109 @@
 from random import randint
 import math
-global Bitcount,PoolSize
+global Bitcount,PoolSize,DecimalPoint,DecimalBin
+DecimalBin = 10
+DecimalPoint =3
 Bitcount = 5
 PoolSize = 10
-class Chromosome:
+string = "here the function is F = x^3-220x^2+1600x-480000"
 
+class Chromosome:
+    '''
+    Self.Genes = [[WholeNumberPart],[DecimalPart]]
+
+    '''
     def __init__(self,Value):# runs on intitiaion
         self.Fitness = 0 # directly proportional to the ideality of the chromosome
-        self.Genes = Value 
+        self.Genes = Value
         
         # Genes[0] = 1 if positive number or 0 if negative
         # Genes[1:5] if the number in binary format             
     def Value(self):
         #The Value of genes
-        if self.Genes != 'Null':
+        if self.Genes[0] != 'Null':
             decimal = 0
-            for i in range(-1,-Bitcount-1,-1):
-                decimal += self.Genes[1:][i]*(2**(abs(i)-1))
-            decimal *= (-1)*((-1)**self.Genes[0])
+            fraction = 0
+
+            for i in range(-1,-Bitcount-1,-1):#computing the whole part
+                
+                decimal += self.Genes[0][1:][i]*(2**(abs(i)-1))
+
+            for i in range(-1,-DecimalBin-1,-1):#computing the Fraction part
+
+                fraction += (self.Genes[1][1:][i]*(2**(abs(i)-1)))
+            decimal+=fraction/(10**DecimalPoint)#Adding them together
+            decimal *= (-1)*((-1)**self.Genes[0][0])#Taking care of the sign
+            decimal = float("{:.3f}".format((decimal)))#Clipping to required decimal places
             return decimal
         else:
             return 'Null'
 
     def __add__(self, other):#single point crossover
-        cross_over_point = randint(1,Bitcount)
+        #Crossover for the whole part and fraction part thru loop
+        cross_over_point = [randint(1,Bitcount),randint(1,DecimalBin)]
+        child1 = []
+        child2 = []
 
-        child1 = self.Genes[:cross_over_point]
-        for i in other.Genes[cross_over_point:]:
-            child1.append(i)
-        child2 = other.Genes[:cross_over_point]
-        for i in self.Genes[cross_over_point:]:
-            child2.append(i)
+        for k in (0,1):
+            child1.append(self.Genes[k][:cross_over_point[k]])
+            for i in other.Genes[k][cross_over_point[k]:]:
+                child1[k].append(i)
+
+            child2.append(other.Genes[k][:cross_over_point[k]])
+
+            for i in self.Genes[k][cross_over_point[k]:]:
+                child2[k].append(i)
 
 
+        #print(child2)
         #MUTATION
         mutation = randint(0,100)
         if mutation > 40:
+            flip_Bit = [randint(1,Bitcount),randint(1,DecimalBin)]
+            for k in (0,1):
+                
 
-            flip_Bit = randint(0,Bitcount)
-            child1[flip_Bit] = 1-child1[flip_Bit]
-            flip_Bit = randint(0,Bitcount)
-            child2[flip_Bit] = 1-child2[flip_Bit]
+                child1[k][flip_Bit[k]] = 1-(child1[k][flip_Bit[k]])
+                child2[k][flip_Bit[k]] = 1-(child2[k][flip_Bit[k]])
+        
         return (Chromosome(child1),Chromosome(child2))
                 
 def Population():
 
 
-    raw_pool = ([[randint(0,1) for x in range(Bitcount+1)] for i in range(10)])
-    res = []
-    for i in raw_pool:
+    raw_poolW = ([[randint(0,1) for x in range(Bitcount+1)] for i in range(10)])
+    x = randint(0,999)
+    
+    raw_poolD =  ([[randint(0,1) for x in range(DecimalBin+1)] for i in range(10)])
 
+    raw_pool = []
+
+    for i in range(0,len(raw_poolW)):
+        raw_pool.append([raw_poolW[i],raw_poolD[i]])
+
+    res = []
+
+
+    for i in raw_pool:
+        
         if i not in res:
             res.append(i)
     
 
     while len(res) != len(raw_pool):
-        x = [randint(0,1) for x in range(Bitcount+1)]
+        raw_poolW = ([[randint(0,1) for x in range(Bitcount+1)]])
+        #raw_poolD = ([int(i) for i in bin(randint(0,int('9'*DecimalPoint)))[2:]])
+        raw_poolD = ([[randint(0,1) for x in range(DecimalBin+1)]])
+        x = [[raw_poolW[i],raw_poolD[i]]]
         if x not in res:
             res.append(x)
             
 
+
     pool = []
 
 
-    for i in res:
-        x = []
-        for j in i:
-            x.append(int(j))
+    for x in res:
         pool.append(Chromosome(x))
     return pool
     
@@ -74,9 +111,10 @@ def Population():
 def Fitness_Function(chromosome):
     x = chromosome.Value()
 
-    #here the function is F = x^3-220x^2+1600x-480000
+    
     #find maximum Value of it
     chromosome.Fitness = x**3-(220*(x**2))+(1600*x)-480000
+    
 
 def Selection(Pool):
     
@@ -106,9 +144,45 @@ def Selection(Pool):
 Pool = Population()
 
 selection = Selection(Pool)
+#------------------------------PRETTY PRINT-----------------------------------
+data1 = [['WHOLE No CHROMOSOME'],['FRACTION CHROMOSOME'],['VALUE'],['FITNESS']]
+columns = []
+data = []
 for i in selection:
-    print(i.Genes,'___________',i.Value(),'__________',i.Fitness)
-print('-----------------------------------------')
+    data.append([i.Genes[0],i.Genes[1],i.Value(),i.Fitness])
+for i in data1:
+    columns.append(i[0])
+width = [0,0,0,0]
+data.insert(0,tuple(columns))
+
+for row in data:
+    t = 0
+    for column in row:
+        if width[t] < len(str(column)):
+            width[t] = len(str(column))
+        t+=1
+        
+for row in data:
+    t=0
+    
+    for column in row:
+        x = width[t]
+        
+        for i in str(column):
+            print (i,end = '')
+            x-=1
+            
+        while x != 0:
+           print(' ',end = '')
+           x-=1
+        t+=1
+        print(' | ',end ='')
+    print()
+        
+print()
+print()
+
+#----------------------------------DRIVER CODE-----------------------------------
 End_Pool = []
 generations = 0
 while True:
@@ -136,6 +210,7 @@ while True:
         else:
             End_Pool = []
 for i in selection:
+    print(string)
     print('Maximum at dependent variable :',i.Value(),' with function value: ',i.Fitness)
     break   
 print("Generations taken: ",generations)
